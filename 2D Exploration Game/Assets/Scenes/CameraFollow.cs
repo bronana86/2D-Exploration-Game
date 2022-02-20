d56e023341c573fb95039f3b2529a682a4112f40
Trevor Konty <trevkonty@gmail.com>
2022-02-20 12:33:28 -0500

    Add first room, player movement, and camera follow. Problems with camera follow.

---
+++ 2D Exploration Game/Assets/Scenes/CameraFollow.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01b29490e3d6a6512ce69ac5d7f0a2fd6d934c6da546135866e06bd0759ad91e
+size 301
---
+++ 2D Exploration Game/Assets/Scenes/Player.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6b589b1cfcd8ffd76a56c57b486eda6c1b2f1723cae94f020281aa4074d18fb9
+size 1311
